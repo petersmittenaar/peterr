63f9d93ca63c029f542043890d4d364407b1c25c
Peter Smittenaar <peter@smittenaar.uk>
2025-06-04 19:48:07 +0100

    Add test for CorGetNStrongest

---
+++ tests/testthat/test-cor_get_n_strongest.r
@@ -0,0 +1,10 @@
+context('Get N strongest correlations')
+
+test_that('CorGetNStrongest returns expected columns and rows', {
+    df <- data.frame(a = 1:5,
+                     b = c(5,4,3,2,1),
+                     c = c(2,3,4,5,6))
+    res <- CorGetNStrongest(df, n = 2)
+    expect_equal(colnames(res), c('var1', 'var2', 'r'))
+    expect_equal(nrow(res), 2)
+})
